01a78c2f9931dc5782f7ce5b92b694a51fb0021d
cristhianvr24 <cristhian24vr@gmail.com>
2022-05-25 19:16:21 -0400

    base de datos

---
+++ database/migrations/2022_05_25_105427_create_estudiante_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEstudianteTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('estudiante', function (Blueprint $table) {
+            $table->biginteger('cedula')->unsigne()->unique();
+            $table->string('nombre_1');
+            $table->string('nombre_2')->nullable();
+            $table->string('apellido_1');
+            $table->string('apellido_2')->nullable();
+            $table->string('telefono');
+            $table->string('correo')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('estudiante');
+    }
+}
